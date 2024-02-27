c84de5082c8b220b88bccabc80e7ee7a1d64fdbc
ui-abrarul <abrarulh18@gmail.com>
2024-02-27 14:33:09 +0530

    updated

--- schoolsapp/frontend/schools/src/components/ShowSchool.js
+++ schoolsapp/frontend/schools/src/components/ShowSchool.js
@@ -1,61 +1,61 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import NoDataImage from '../assets/no-data-found.png'
 
 const ShowSchool = () => {
-
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8081')
-    .then(res => {
-      setData(res.data)
-    })
-    .catch(err => console.log(err)) 
-  },[])
-
-  console.log(data)
-
-
+    axios
+      .get("http://localhost:8081")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div className='page-container'>
-
-<div className="page-title">
-        <h2>Schools List</h2>
-        <Link to='/register'>Add School</Link>
+    <div className="page-container">
+      <div className="page-title">
+       {data.length > 0 && <h2>Schools List</h2>}
+        <Link to="/register">Add School</Link>
       </div>
 
-<div className="card-list">
-      {data.map((item,index) => (
+     {data.length ? <div className="card-list">
+         { data.map((item, index) => (
+          <article className="card" key={item.id}>
+            <div className="card-header">
+              <img
+                src={`http://localhost:8081/images/` + item.image}
+                alt={item.name}
+              />
+            </div>
+            <div className="card-body">
+              <small>{item.city}</small>
+              <h2>{item.name}</h2>
 
-          <article className='card' key={item.id}>
+              <ul>
+              <li>
+                {item.contact}
+                </li>
+                <li>
+                {item.email_id}
+                </li>
+                <li>
+                {item.address}
+                </li>
+                <li>
+                {item.state}
+                </li>
+              </ul>
 
-<div className='card-header'>
-<img src={`http://localhost:8081/images/`+item.image} alt={item.name} />
-
-  </div>
-  <div className="card-body">
-     <small>{item.city}</small>
-		<h2>{item.name}</h2>
-
-    <p>
-      {item.address}
-    </p>
-      <p>{item.email_id}</p>
-      <p>{item.contact}</p>
-      <p>{item.state}</p>
-	</div>
-
-
-
+            </div>
           </article>
-       
-      
-      ))}
-</div>
+        ) ) }
+      </div> : <img className="fallback-image" src={NoDataImage} alt="No Data Found"/>}
     </div>
-  )
-}
+  );
+};
 
-export default ShowSchool
+export default ShowSchool;
